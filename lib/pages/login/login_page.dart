d867e310371e3f1732aedaee899953c57e6b34bf
idikim <hjkim9217@naver.com>
2024-11-22 16:33:01 +0900

    feat: UI 구조 형성

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,8 +1,61 @@
+import 'package:dino_shop/theme.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Text('LoginPage');
+    return MaterialApp(
+      home: LoginPage(),
+      themeMode: ThemeMode.light,
+      theme: theme,
+      darkTheme: darkTheme,
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Image.asset('assets/images/application/logo.png'),
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'EMAIL'),
+            ),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'PASSWORD'),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print('이메일: ${_emailController.text}');
+                  print('비밀번호: ${_passwordController}');
+                },
+                child: Text('LOGIN'))
+          ],
+        ),
+      ),
+    );
   }
 }
