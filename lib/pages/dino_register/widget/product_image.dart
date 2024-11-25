63dfa6f1732ec17cffaeff8754bcf58d24332718
imeujin <imeujin.dev@gmail.com>
2024-11-25 15:20:13 +0900

    feat: 이미지 선택 및 취소 기능 추가
    - image_picker 사용하여 갤러리 접근

--- lib/pages/dino_register/widget/product_image.dart
+++ lib/pages/dino_register/widget/product_image.dart
@@ -1,53 +1,93 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ProductImage extends StatelessWidget {
+class ProductImage extends StatefulWidget {
+  @override
+  State<ProductImage> createState() => _ProductImageState();
+}
+
+class _ProductImageState extends State<ProductImage> {
+  final picker = ImagePicker();
+  List<XFile?> selectedImages = []; // 갤러리에서 선택한 이미지
+  List<XFile?> images = []; // 보여질 이미지들
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
+          getImageBox(),
+          ...List.generate(images.length, (int i) => imageBox(i))
+        ],
+      ),
+    );
+  }
+
+  /// 갤러리에서 이미지 가져올 위젯
+  Container getImageBox() {
+    return Container(
+      height: 64,
+      width: 64,
+      child: IconButton(
+        onPressed: () async {
+          // image picker 로 갤러리에서 가져온 이미지
+          selectedImages = await picker.pickMultiImage();
+          setState(() {
+            // 갤러리에서 가져온 이미지들 images 에 추가
+            images.addAll(selectedImages);
+          });
+        },
+        icon: Icon(Icons.camera_alt_outlined),
+        color: Colors.grey,
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(8),
+      ),
+    );
+  }
+
+  /// 선택한 이미지 위젯
+  Padding imageBox(int index) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          // 이미지 박스
           Container(
             height: 64,
             width: 64,
-            child: Icon(
-              Icons.camera_alt_outlined,
-              color: Colors.grey,
-            ),
             decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
               borderRadius: BorderRadius.circular(8),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                // images 리스트에 있는 사진 순서대로 보여줌
+                image: FileImage(File(images[index]!.path)),
+              ),
             ),
           ),
-          SizedBox(width: 20),
-          Stack(
-            children: [
-              Container(
-                height: 64,
-                width: 64,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Image.asset(
-                    '/Users/t2024-m0194/imyujin/workspace/dino_shop/assets/images/product/01.jpg',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                ),
+          // 취소 버튼
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                // x 버튼 누르면 images 에서 해당 사진 지우기
+                images.remove(images[index]);
+              });
+            },
+            child: Container(
+              child: Icon(
+                Icons.close,
+                color: Colors.black,
+                size: 15,
               ),
-              Positioned(
-                left: 40,
-                top: 40,
-                child: CloseButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                      iconColor: WidgetStatePropertyAll(Colors.blue),
-                      iconSize: WidgetStatePropertyAll(20)),
-                ),
-              )
-            ],
-          )
+              decoration: BoxDecoration(
+                color: Color(0xFFFFF068),
+              ),
+            ),
+          ),
         ],
       ),
     );
