9f45264be79a233353e24327bd63a3cac7c69b74
cjaeseok <wotjrdk123@gmail.com>
2024-11-26 18:03:02 +0900

    bugfix/login_keyboard

--- lib/pages/login/widgets/log_in_box.dart
+++ lib/pages/login/widgets/log_in_box.dart
@@ -9,16 +9,15 @@ class Manager {
 
   // 생성자를 통해 관리자 계정을 초기화
   Manager({
-    required this.email,    //필수 입력값 required, null값 문제 방지
+    required this.email, //필수 입력값 required, null값 문제 방지
     required this.password,
   });
 
-  /// 관리자 계정인지 확인하는 메서드
+  /// 관리자 계정인지 확인
   bool isManager(String inputEmail, String inputPassword) {
     return inputEmail == email && inputPassword == password;
   }
 }
-
 
 class LogInBox extends StatelessWidget {
   final _emailController = TextEditingController();
@@ -26,11 +25,9 @@ class LogInBox extends StatelessWidget {
 
   final VoidCallback onLoginSuccess;
 
+  LogInBox({required this.onLoginSuccess});
 
-  LogInBox({
-    required this.onLoginSuccess});
-
-    // 관리자 계정을 정의
+  // 관리자 계정을 정의
   final Manager manager = Manager(
     email: "admin@gmail.com",
     password: "1234",
@@ -38,53 +35,59 @@ class LogInBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // 아이디, 비밀번호 입력
-        _buildLoginTextField(),
+    return GestureDetector(           //키보드 사라지게 만들고 싶은데...안되네여?
+      behavior: HitTestBehavior.opaque, //안되서 
+      onTap: () {
+        // 화면 클릭 시 키보드 닫기
+        FocusScope.of(context).unfocus();
+      },
+      child: Column(
+        children: [
+          // 아이디, 비밀번호 입력
+          _buildLoginTextField(),
 
-        SizedBox(height: 60),
+          SizedBox(height: 60),
 
-        // 로그인 버튼
-        ElevatedButton(
-          onPressed: () {
-            final email = _emailController.text;
-            final password = _passwordController.text;
+          // 로그인 버튼
+          ElevatedButton(
+            onPressed: () {
+              final email = _emailController.text;
+              final password = _passwordController.text;
 
-            // 관리자 계정 확인
-            if (manager.isManager(email, password)) {
-              // 관리자 계정 로그인 성공 시
-              _showCustomDialog(
-                context,
-                title: "관리자님!",
-                content: "오늘 하루도 열심히.",
-                onDialogClose: onLoginSuccess, // 팝업 닫힌 후 다음 페이지 이동
-              );
-            } 
-            // 일반 계정 확인
-            else if (_validateCredentials(email, password)) {
-              // 일반 계정 로그인 성공 시
-              _showCustomDialog(
-                context,
-                title: "환영합니다!",
-                content: "로그인이 성공적으로 완료되었습니다.",
-                onDialogClose: onLoginSuccess, // 팝업 닫힌 후 다음 페이지 이동
-              );
-            } else {
-              // 로그인 실패 처리
-              _showCustomDialog(
-                context,
-                title: "Login Error",
-                content: "아이디와 비밀번호를 확인해주세요.",
-              );
-            }
-          },
-          child: const Text("LOGIN"), // 버튼 텍스트
-        ),
-      ],
+              // 관리자 계정 확인
+              if (manager.isManager(email, password)) {
+                // 관리자 계정 로그인 성공 시
+                _showCustomDialog(
+                  context,
+                  title: "관리자님!",
+                  content: "오늘 하루도 열심히.",
+                  onDialogClose: onLoginSuccess, // 팝업 닫힌 후 다음 페이지 이동
+                );
+              }
+              // 일반 계정 확인
+              else if (_validateCredentials(email, password)) {
+                // 일반 계정 로그인 성공 시
+                _showCustomDialog(
+                  context,
+                  title: "환영합니다!",
+                  content: "로그인이 성공적으로 완료되었습니다.",
+                  onDialogClose: onLoginSuccess, // 팝업 닫힌 후 다음 페이지 이동
+                );
+              } else {
+                // 로그인 실패 처리
+                _showCustomDialog(
+                  context,
+                  title: "Login Error",
+                  content: "아이디와 비밀번호를 확인해주세요.",
+                );
+              }
+            },
+            child: const Text("LOGIN"), // 버튼 텍스트
+          ),
+        ],
+      ),
     );
   }
-
 
   Column _buildLoginTextField() {
     return Column(
@@ -103,13 +106,13 @@ class LogInBox extends StatelessWidget {
     );
   }
 
-    bool _validateCredentials(String email, String password) {
+  bool _validateCredentials(String email, String password) {
     return email.isNotEmpty && password.isNotEmpty; // 비어 있지 않은지 확인
   }
 
   // 팝업 창 표시
   void _showCustomDialog(BuildContext context,
-      {required String title, 
+      {required String title,
       required String content,
       VoidCallback? onDialogClose}) {
     showDialog(
@@ -132,4 +135,3 @@ class LogInBox extends StatelessWidget {
     );
   }
 }
-
