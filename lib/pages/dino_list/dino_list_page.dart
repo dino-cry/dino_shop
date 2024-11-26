828ae71f291cff8c29f0075b8b56369de1e54061
LeeHJuu <xyident903@gmail.com>
2024-11-26 19:24:20 +0900

    feat: 상품 가격순 정렬

--- lib/pages/dino_list/dino_list_page.dart
+++ lib/pages/dino_list/dino_list_page.dart
@@ -24,6 +24,9 @@ class _DinoListPageState extends State<DinoListPage> {
   // 좋아요 상태 리스트 (각 제품마다 별도의 좋아요 상태를 관리)
   late List<bool> screenStars;
 
+  // 가격 정렬 상태 관리
+  bool isAscending = true;
+
   @override
   void initState() {
     super.initState();
@@ -35,9 +38,42 @@ class _DinoListPageState extends State<DinoListPage> {
     // productList.removeRange(0, productList.length);
     return Scaffold(
       appBar: CustomAppBar(title: APP_TITLE),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: productList.isEmpty ? NoItemsMessage() : GridViewProductList(),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              //위아래 화살표
+              isAscending
+                  ? Icon(Icons.arrow_downward)
+                  : Icon(Icons.arrow_upward),
+              //가격 정렬 텍스트 버튼
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    isAscending = !isAscending;
+                    productList.sort((a, b) => isAscending
+                        ? a.price.compareTo(b.price)
+                        : b.price.compareTo(a.price));
+                  });
+                },
+                child: Text(isAscending ? '낮은 가격순' : '높은 가격순'),
+              ),
+            ],
+          ),
+          // 실선
+          Container(
+              width: 500, child: Divider(color: Colors.black, thickness: 2.0)),
+          Expanded(
+            // Expanded로 GridView를 감쌈
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: productList.isEmpty
+                  ? NoItemsMessage()
+                  : GridViewProductList(),
+            ),
+          ),
+        ],
       ),
 
       // 상품 추가 플로팅 버튼
